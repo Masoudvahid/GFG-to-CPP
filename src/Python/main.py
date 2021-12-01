4cfedb6c08c00e767b2a072e8c8c6f237e996e4f
Masoudvahid <83933253+Masoudvahid@users.noreply.github.com>
2021-12-01 16:47:29 +0300

    Update main.py

--- src/Python/main.py
+++ src/Python/main.py
@@ -3,7 +3,7 @@ from googlesearch import search
 
 keyword = input("Enter a keyword to search: ")
 keyword += " code site:geeksforgeeks.org"
-file = open("../search_results/Links.txt", 'w')
+file = open("search_results/Links.txt", 'w')
 urls = search(keyword)
 for url, index in zip(urls, range(1, 10)):
     file.write(url + '\n')
@@ -16,7 +16,7 @@ from bs4 import BeautifulSoup
 import requests
 
 # Read and save webpage from website
-file = open("../search_results/Links.txt", 'r')
+file = open("search_results/Links.txt", 'r')
 link_index = int(input("Enter link index: "))
 index = 1
 for link in file:
@@ -29,7 +29,7 @@ page = requests.get(URL.replace('\n', '')).text
 soup = BeautifulSoup(page, 'lxml')
 index = 0
 for code_container in soup.find_all('div', class_='container'):
-    file = open("../search_results/Codes/code" + str(index) + ".txt", 'a')
+    file = open("search_results/Codes/code" + str(index) + ".txt", 'a')
     for lines_of_code in code_container.find_all('div'):
         file.write(lines_of_code.text + "\n")
     file.close()
@@ -41,8 +41,8 @@ for code_container in soup.find_all('div', class_='container'):
 import subprocess
 
 subprocess.call(
-    ["g++", "-std=c++17", "../src/cpp/main.cpp", "-o",
-     "../src/cpp/a.out"])
-subprocess.call("../src/cpp/./a.out")
+    ["g++", "-std=c++17", "src/cpp/main.cpp", "-o",
+     "src/cpp/a.out"])
+subprocess.call("src/cpp/./a.out")
 
 # Third part of the program
