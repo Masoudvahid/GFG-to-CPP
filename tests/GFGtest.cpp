b270d80ac5af39addbffab2255f246ebf3f05af1
Masoudvahid <83933253+Masoudvahid@users.noreply.github.com>
2021-12-21 19:16:33 +0300

    Update GFGtest.cpp

--- tests/GFGtest.cpp
+++ tests/GFGtest.cpp
@@ -1,6 +1,53 @@
 #include <gtest/gtest.h>
-#include "../GFG_operations.h"
+#include "../src/GFG_operation.cpp"
 
+TEST(TestGFG, LinksExists) {
+	FILE* f = fopen("../search_results/Links.txt", "r");
+	ASSERT_TRUE(f != NULL);
+}
+
+TEST(TestGFG, CodesFolderExists) {
+	FILE* f = fopen("../search_results/Codes", "r");
+	ASSERT_TRUE(f != NULL);
+}
+
+TEST(TestGFG, CodesExists) {
+	GFG_operations gfg();
+	FILE* f = fopen("../search_results/Codes/0_C++.txt", "r");
+	ASSERT_TRUE(f != NULL);
+	f = fopen("../search_results/Codes/1_php.txt", "r");
+	ASSERT_TRUE(f != NULL);
+	f = fopen("../search_results/Codes/2_C#.txt", "r");
+	ASSERT_TRUE(f != NULL);
+	f = fopen("../search_results/Codes/3_php.txt", "r");
+	ASSERT_TRUE(f != NULL);
+	f = fopen("../search_results/Codes/4_script.txt", "r");
+	ASSERT_TRUE(f != NULL);
+}
+
+TEST(TestGFG, LinksExists) {
+	FILE* f = fopen("../search_results/Links.txt", "r");
+	ASSERT_TRUE(f != NULL);
+}
+
+TEST(TestGFG, CodesFolderExists) {
+	FILE* f = fopen("../search_results/Codes", "r");
+	ASSERT_TRUE(f != NULL);
+}
+
+TEST(TestGFG, CodesExists) {
+	GFG_operations gfg();
+	FILE* f = fopen("../search_results/Codes/0_C++.txt", "r");
+	ASSERT_TRUE(f != NULL);
+	f = fopen("../search_results/Codes/1_php.txt", "r");
+	ASSERT_TRUE(f != NULL);
+	f = fopen("../search_results/Codes/2_C#.txt", "r");
+	ASSERT_TRUE(f != NULL);
+	f = fopen("../search_results/Codes/3_php.txt", "r");
+	ASSERT_TRUE(f != NULL);
+	f = fopen("../search_results/Codes/4_script.txt", "r");
+	ASSERT_TRUE(f != NULL);
+}
 TEST(TestGFG, LinksExists) {
 	FILE* f = fopen("../search_results/Links.txt", "r");
 	ASSERT_TRUE(f != NULL);
